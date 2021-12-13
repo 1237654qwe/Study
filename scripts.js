e644775cef378b58178573b0a6f70751bfac9d7e
kalibr99999@gmail.com <kalibr99999@gmail.com>
2021-12-13 15:30:50 +0300

    Убрал лишнюю вложенность

--- scripts.js
+++ scripts.js
@@ -1,69 +1,72 @@
 const arr = ["asd", "afffd", "cc", "kk"]
 
-    const compare = (arr) => {
+const compare = (arr) => {
 
-        newArr = []
+    newArr = []
 
-        arr.forEach((item, i) => {
-            if (i + 1 < arr.length) {
-                if (item[0] === arr[i + 1][0] && item[item.length - 1] === arr[i + 1][(arr[i + 1]).length - 1]) {
-                    newArr.push(true)
+    arr.forEach((item, i) => {
+        if (!(i + 1 < arr.length)) {
+            return
+        }
+        if (item[0] === arr[i + 1][0] && item[item.length - 1] === arr[i + 1][(arr[i + 1]).length - 1]) {
+            newArr.push(true)
+        } else {
+            newArr.push(false)
+        }
+
+    })
+    return newArr
+}
+
+console.log(compare(arr))
+
+const arr2 = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]]
+
+const likeFlat = (arr) => {
+    return arr.reduce((acc, item) => {
+        if (Array.isArray(item)) {
+            return [...acc, ...likeFlat(item)]
+        } else {
+            return [...acc, item]
+        }
+    }, [])
+}
+
+console.log(likeFlat(arr2))
+
+const arr3 = [-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]
+
+const transform = (arr) => {
+
+    let newArr = []
+    let prepArr = []
+
+    arr.forEach((item, i) => {
+        const lastEl = prepArr[prepArr.length - 1]
+        if (!lastEl) {
+            return prepArr.push(item)
+        }
+
+        if (lastEl + 1 === item) {
+            prepArr.push(item)
+            if (i + 1 === arr.length) {
+                if (prepArr.length >= 3) {
+                    newArr = [...newArr, `${prepArr[0]} - ${prepArr[prepArr.length - 1]}`]
                 } else {
-                    newArr.push(false)
+                    newArr = [...newArr, ...prepArr]
                 }
             }
-        })
-        return newArr
-    }
-
-    console.log(compare(arr))
-
-    const arr2 = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]]
-
-	const likeFlat = (arr) => {
-		return arr.reduce((acc, item) => {
-			if (Array.isArray(item)) {
-				return [...acc, ...likeFlat(item)]
-			} else {
-				return [...acc, item]
-			}
-		}, [])
-	}
-
-	console.log(likeFlat(arr2))
-
-    const arr3 = [-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]
-
-    const transform = (arr) => {
-
-        let newArr = []
-        let prepArr = []
-
-        arr.forEach((item, i) => {
-            const lastEl = prepArr[prepArr.length - 1]
-            if (lastEl) {
-                if (lastEl + 1 === item) {
-                    prepArr.push(item)
-                    if (i + 1 === arr.length) {
-                        if (prepArr.length >= 3) {
-                            newArr = [...newArr, `${prepArr[0]} - ${prepArr[prepArr.length - 1]}`]
-                        } else {
-                            newArr = [...newArr, ...prepArr]
-                        }
-                    }
-                } else {
-                    if (prepArr.length >= 3) {
-                        newArr = [...newArr, `${prepArr[0]} - ${prepArr[prepArr.length - 1]}`]
-                    } else {
-                        newArr = [...newArr, ...prepArr]
-                    }
-                    prepArr = [item]
-                }
+        } else {
+            if (prepArr.length >= 3) {
+                newArr = [...newArr, `${prepArr[0]} - ${prepArr[prepArr.length - 1]}`]
             } else {
-                prepArr.push(item)
+                newArr = [...newArr, ...prepArr]
             }
-        })
-        return newArr.join()
-    }
+            prepArr = [item]
+        }
 
-    console.log(transform(arr3))
+    })
+    return newArr.join()
+}
+
+console.log(transform(arr3))
